da0c239f20c5eebca5bd49549754bf642b6b427f
georgegyan <georgegyankwame72@gmail.com>
2025-11-26 13:06:20 +0000

    Fix database configuration and Python version for Render

--- backend/mmesa_survey_backend/settings.py
+++ backend/mmesa_survey_backend/settings.py
@@ -65,14 +65,10 @@ WSGI_APPLICATION = 'mmesa_survey_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3')
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
 }
-
-CORS_ALLOW_ALL_ORIGINS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
