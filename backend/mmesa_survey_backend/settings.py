e9a38a4f0b026b82748e5745b94f53c16a7a8f87
georgegyan <georgegyankwame72@gmail.com>
2025-11-26 18:50:26 +0000

    Use sqlite temporily to fix development

--- backend/mmesa_survey_backend/settings.py
+++ backend/mmesa_survey_backend/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://mmesa-survey.vercel.app"
+    "https://mmesa-survey-system.vercel.app"
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
